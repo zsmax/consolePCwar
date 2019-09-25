fa21ca708ecca0945aede402ca060fa598ff849f
maxim maniak <maxim.maniak@gmail.com>
2019-09-25 19:37:06 +0300

    printing message added

---
+++ src/js/game/printMessage.js
@@ -0,0 +1,25 @@
+/**
+ * Styles for messaging in chat
+ * printing message in game field
+ * @param {String} who add this class for message
+ * @param {String} text message
+ */
+export function printMessage(who, text) {
+
+    const classNames = {
+        system: 'system',
+        pc: 'pc',
+        player: 'player'
+    };
+
+    const gameField = document.querySelector('.gameField');
+    let message = document.createElement('div');
+    message.textContent = text;
+
+    if (classNames.hasOwnProperty(who)) {
+        message.classList.add(classNames[who]);
+        gameField.appendChild(message);
+    } else {
+        console.warn('error');
+    }
+}
