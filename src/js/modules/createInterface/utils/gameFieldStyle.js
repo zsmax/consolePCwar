66992370e34945410b8eba8a1b7f908912679089
maxim maniak <maxim.maniak@gmail.com>
2019-09-28 14:11:16 +0300

    remove all styles

--- src/js/modules/createInterface/utils/gameFieldStyle.js
+++ src/js/modules/createInterface/utils/gameFieldStyle.js
@@ -1,8 +1,11 @@
-export const gameFieldStyle =
-    'width: 390px; ' +
-    'height: 450px; ' +
-    'margin: 0 auto; ' +
-    'overflow-y: auto; ' +
-    'margin-top: -70px; ' +
-    'color: wheat; ' +
-    'padding-bottom: 20px;';
+export const gameFieldStyle = `
+display: flex;
+flex-direction: column;
+padding: 0 10px 20px 10px;
+width: 390px;
+height: 450px;
+margin: 0 auto;
+overflow-y: auto;
+margin-top: -70px;
+color: wheat;
+`;
